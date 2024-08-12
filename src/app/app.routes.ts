c687b1c3b07c9d9a863024cd9444558eeb2be95b
Gaurav Goyal <goyalgaurav64@gmail.com>
2024-08-12 08:04:16 +0530

    added and modified files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { BlogsComponent } from './components/blogs/blogs.component';
+import { BlogDetailComponent } from './components/blog-detail/blog-detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        component:HomeComponent
+    },
+    {
+        path:"about",
+        component:AboutComponent
+    },
+    {
+        path:'blogs',
+        component:BlogsComponent
+    },
+    {
+        path:"blog/:id",
+        component:BlogDetailComponent
+    }
+];
---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,16 @@
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  router = inject(Router);
+  AllBlogs(){
+    this.router.navigateByUrl("/blogs");
+  }
+}
